a43ee010cea76ad3a12099dbf7b07543560d2299
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-12-27 17:09:26 +0100

    velka prvocisla, delene, pomale

--- prvocisla/prvocisla.go
+++ prvocisla/prvocisla.go
@@ -1,12 +1,15 @@
 package main
 
 import "fmt"
+import "os"
+import "io"
 
-const MAX = 1e6
+const MAX = 1e12
+const SEEK = 1e8
+const FILE = "prv.txt"
 
 func main() {
-	var sito []bool
-	sito = make([]bool, MAX)
+	sito := make([]bool, SEEK)
 	sito[0] = true
 	for i, t := range sito {
 		n := i + 1
@@ -18,9 +21,48 @@ func main() {
 			}
 		}
 	}
+	var soubor *os.File
+	soubor, _ = os.Create(FILE)
 	for i, t := range sito {
 		if !t {
-			fmt.Printf("%d\n", i+1)
+			str := fmt.Sprintf("%d\n", i+1)
+			soubor.WriteString(str)
 		}
+	}
+	soubor.Close()
+	for i := 1; i <= MAX/SEEK; i++ {
+		var buf int
+		var err error
+		prvocisla := make([]int, 0)
+		soubor, _ = os.Open(FILE)
+		for {
+			_, err = fmt.Fscanf(soubor, "%d", &buf)
+			if err == io.EOF {
+				break
+			}
+			prvocisla = append(prvocisla, buf)
+		}
+		soubor.Close()
+		fmt.Fprintf(os.Stderr, "GO:%d\n", i)
+		//sito := make(map[int]bool, SEEK)
+		sito := make([]bool, SEEK)
+		for _, p := range prvocisla {
+			n := p * (i * SEEK / p)
+			if n <= i*SEEK {
+				n += p
+			}
+			for n <= i*SEEK+len(sito) {
+				index := n - i*SEEK - 1
+				sito[index] = true
+			}
+		}
+		soubor, _ = os.OpenFile(FILE, os.O_APPEND|os.O_WRONLY, 0664)
+		for j, t := range sito {
+			if !t {
+				str := fmt.Sprintf("%d\n", j+1+i*SEEK)
+				soubor.WriteString(str)
+			}
+		}
+		soubor.Close()
 	}
 }
