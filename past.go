60737eb5ffab94759b025c57520f75f415727e7a
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2020-02-15 11:13:01 +0100

    prime => big

--- past.go
+++ past.go
@@ -13,7 +13,8 @@ import (
 )
 
 const FILE = "prvocisla/primes.txt"
-const LIMIT = 1152921504606846976 // 2^60
+
+//const LIMIT = 1152921504606846976 // 2^60
 const ACC = 5
 
 func random() (r uint) {
@@ -26,37 +27,37 @@ func random() (r uint) {
 	return
 }
 
-func isPrime(n uint, isPrimeArray *map[uint]bool) bool {
-	if n == 1 {
+func isPrime(n *big.Int, isPrimeArray *map[uint]bool) bool {
+	if n.Cmp(big.NewInt(1)) == 0 {
 		return false
 	}
-	if (*isPrimeArray)[n] {
+	if (*isPrimeArray)[uint(n.Uint64())] {
 		return true
-	} else if n <= 134217728 {
+	} else if n.Cmp(big.NewInt(134217728)) <= 0 {
 		return false
 	}
-	if n%2 == 0 || n%3 == 0 {
+	if big.NewInt(0).Mod(n, big.NewInt(2)).Cmp(&big.Int{}) == 0 || big.NewInt(0).Mod(n, big.NewInt(3)).Cmp(&big.Int{}) == 0 {
 		return false
 	}
-	for i := uint(5); i*i <= n; i += 6 {
-		if n%i == 0 || n%(i+2) == 0 {
+	for i := big.NewInt(5); big.NewInt(0).Mul(i, i).Cmp(n) <= 0; i.Add(i, big.NewInt(6)) {
+		if big.NewInt(0).Mod(n, i).Cmp(&big.Int{}) == 0 || big.NewInt(0).Mod(n, big.NewInt(0).Add(i, big.NewInt(2))).Cmp(&big.Int{}) == 0 {
 			return false
 		}
 	}
 	return true
 }
 
-func isPrimeProbably(n uint, isPrimeArray *map[uint]bool) bool {
-	if n <= uint(1e12) {
+func isPrimeProbably(n *big.Int, isPrimeArray *map[uint]bool) bool {
+	if n.Cmp(big.NewInt(1e12)) <= 0 {
 		return isPrime(n, isPrimeArray)
 	}
-	if n%2 == 0 || n%3 == 0 {
+	if big.NewInt(0).Mod(n, big.NewInt(2)).Cmp(&big.Int{}) == 0 || big.NewInt(0).Mod(n, big.NewInt(3)).Cmp(&big.Int{}) == 0 {
 		return false
 	}
 
 	for i := 0; i < ACC; i++ {
 		cmd := exec.Command("dc")
-		cmd.Stdin = strings.NewReader(fmt.Sprintf("%d %d^%d%%p\n", random(), n-1, n))
+		cmd.Stdin = strings.NewReader(fmt.Sprintf("%d %d^%d%%p\n", random(), big.NewInt(0).Sub(n, big.NewInt(1)), n))
 		outb, _ := cmd.Output()
 		out := string(outb)
 		var result uint
@@ -72,33 +73,30 @@ func main() {
 	var file *os.File
 	var T uint
 	var primes []uint
+	var isPrimeArray map[uint]bool
 	var err error
-	var buf *big.Int
+	var buf uint
 	rand.Seed(time.Now().Unix())
 
 	fmt.Scanf("%d", &T)
 	file, _ = os.Open(FILE)
 	primes = make([]uint, 0)
+	isPrimeArray = make(map[uint]bool)
 	for {
 		_, err = fmt.Fscan(file, &buf)
-		fmt.Println(buf)
 		if err == io.EOF {
 			break
 		}
-		primes = append(primes, uint(buf.Int64()))
+		primes = append(primes, buf)
+		isPrimeArray[buf] = true
 	}
 
 	for i := uint(1); i <= T; i++ {
 		var N *big.Int
+		N = new(big.Int)
 		var ans *big.Int = big.NewInt(1)
-		fmt.Scanf("%d", &N)
+		fmt.Scan(N)
 		//fmt.Fprintln(os.Stderr, N)
-		if N.Cmp(big.NewInt(LIMIT)) == 1 || N.Cmp(&big.Int{}) == 0 {
-			for j := uint(0); j <= T-i; j++ {
-				fmt.Println("O velky Tung")
-			}
-			return
-		}
 		for j := uint(0); big.NewInt(0).Exp(big.NewInt(int64(primes[j])), big.NewInt(3), nil).Cmp(N) <= 0; j++ {
 			cnt := uint(1)
 			for big.NewInt(0).Cmp(big.NewInt(0).Mod(N, big.NewInt(int64(primes[j])))) == 0 {
@@ -108,19 +106,21 @@ func main() {
 				//fmt.Fprintf(os.Stderr, "%d:%v\n", N, isPrimeArray[N])
 			}
 			ans.Mul(ans, big.NewInt(int64(cnt)))
-			if N.ProbablyPrime(ACC) {
+			if isPrimeProbably(N, &isPrimeArray) {
 				break
 			}
 		}
-		if N.ProbablyPrime(ACC) {
+		if isPrimeProbably(N, &isPrimeArray) {
 			ans.Mul(ans, big.NewInt(2))
 			//fmt.Fprintln(os.Stderr, "Prime")
 		} else if big.NewFloat(0).Sqrt(big.NewFloat(0).SetInt(N)).IsInt() {
-			var pow *big.Float
-			var Ipow *big.Int
-			pow.Sqrt(big.NewFloat(0).SetInt(N)).Int(nil)
+			var pow *big.Float = new(big.Float)
+			var Ipow *big.Int = new(big.Int)
+			//fmt.Fprintln(os.Stderr, N)
+			pow = pow.SetInt64(N.Int64())
+			pow.Sqrt(pow)
 			Ipow, _ = pow.Int(nil)
-			if Ipow.ProbablyPrime(ACC) {
+			if isPrimeProbably(Ipow, &isPrimeArray) {
 				ans.Mul(ans, big.NewInt(3))
 				//fmt.Fprintln(os.Stderr, "PrimePow")
 			}
